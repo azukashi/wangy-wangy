3a1138aa47cb007c81aa5159e823c276c55b45fe
Gifaldy Azka <me@falcxxdev.ml>
2022-08-06 20:37:18 +0700

    fix: remove lorem text

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -10,9 +10,7 @@ export default {
 
 <template>
     <p>3. Preview (Silahkan copy)</p>
-    <Textarea
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ex minima enim vero eligendi iste rerum obcaecati pariatur natus omnis consequuntur veritatis molestiae adipisci temporibus sint culpa repellat quas, provident earum quod porro necessitatibus nesciunt fugiat incidunt! Minima alias, amet corrupti magni, accusantium asperiores dolores debitis exercitationem modi omnis, harum voluptatem dolorum nihil aut nisi obcaecati architecto. Quas, earum. Iusto corporis dicta vitae ea minima provident debitis nam tenetur ratione ipsum eum tempore cumque aliquam, itaque quo nostrum distinctio totam at aperiam vel repellat nisi! Adipisci, esse culpa illum explicabo dolorum, eos nobis, accusantium atque nesciunt commodi et repudiandae voluptate."
-    ></Textarea>
+    <Textarea text=""></Textarea>
     <div class="btn-wrapper">
         <Button class="space">Hapus Hasil</Button>
         <Button>Copy</Button>
