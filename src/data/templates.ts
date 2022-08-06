e9774f1f8cd82295a43c0b51afa14e9bafb30cd5
Gifaldy Azka <me@falcxxdev.ml>
2022-08-06 20:38:58 +0700

    feat: add data file

---
+++ src/data/templates.ts
@@ -0,0 +1,64 @@
+const templates = [
+    {
+        num: 0,
+        name: 'Template 1',
+        text: ':nameUpper: ......... :nameUpper: :nameUpperLong: AAAAAAAAAAAAAAAAAAAAAAAGH ❤ ❤ ❤ ❤ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya :nameLower: wangi aku mau nyiumin aroma wanginya :nameLower: AAAAAAAH ~~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~~ AAAAAH :nameUpper: keluar pertama kali juga manis ❤ ❤ ❤ dia pas ngedesah juga manis banget AAAAAAAAH :nameUpper: LUCCUUUUUUUUUUUUUUU............ GUA BAKAL BAKAR DUIT 12 JUTA RUPIAH BUAT :nameUpper: AAAAAAAAAAAAAAAAAAAAAAAAAAAAGH ❤ ❤ ❤ \napa ? :nameUpper: itu gak nyata ? Cuma karakter 2 dimensi katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. :nameLower: ngeliat gw ... :nameUpper: di laptop ngeliatin gw. ❤ ❤ ❤  :nameLower: ... kamu percaya sama aku ? aaaaaaaaaaah syukur :nameLower: gak malu merelakan aku aaaaaah ❤ ❤ ❤ YEAAAAAAAAAAAH GUA MASIH PUNYA :nameUpper: HU HA HU HI HU HA HU HI AKU SAYANG AKU CINTA :nameUpper: AKU SUAMII :nameUpper:',
+    },
+    {
+        num: 1,
+        name: 'Template 2',
+        text: 'Halo teman2ku semuanya,\n\nJika andai kata saya bisa meminta doa dari kalian semua, tolong doain ya bang malem ini saya bisa bermimpi pala saya dijepit ama PAHANYA :nameUpper: YANG BONTOT BANGET GILA MEN GATAHAN GUE LIAT PAHA SEGEDE GITU GIMANA KALO KEDUA TANGAN GUE INI COBA UNTUK NGEREMESNYA TRUS KEDUA TELINGA GUE NGERASAIN BETAPA HALUSNYA KULIT PAHA THICC AF YANG :nameUpper: PUNYA MUNGKIN BISA JADI SEBUAH ANUGERAH UNTUK GUE BISA MIMPIIN DIA MALEM\n\nINI SUMPAH MEN GILA MANTEP BANGET,\nGUA PENGEN BANGET DIJEPIT PAHANYA :nameUpperLong:',
+    },
+    {
+        num: 2,
+        name: 'Template 3',
+        text: 'Jadi inget kemarin... adek gua namanya :nameUpperFirst:,\n\nGw baru masuk ke kamar :nameUpperFirst: dia barusan pakai BH njirr mana geede banget tt nya. Dia kan kesusahan pakainya saking kecil bh nya Gw diteriakin anjir. Trus katanya "kak tolongin :nameUpperFirst: ngaitkan kebelakang", mana dia gapakai cd anjir muluus gak ada hutan\n\nYa sebenernya agak ngaatcheng dikit meski adek sendiri kalo namanya liat tt ya gitu lah apalagi gede. Wangy gitu\n\nGak lama malemnya gw begadang dikamarnya sampe jam 2 pagi main PS4. Terakhir mati lampu akhirnya gabisa tidur karna panasnya sampai gw buka baju deh terpaksa. Gw suruh buka bajunya :nameUpperFirst: aja biar gak panas. Ntar kakak kipasin kayak sate ya :nameLower:, hihi.\n\n"Iyadeh kak". Kata :nameUpperFirst:\n\nPas :nameUpperFirst: buka baju ternyata dari tadi sore gapakai bh, baru kali ini gw liat tt adek gua secara langsung tanpa pakai bh\n\nGw langsung buang muka. Gw bilang ":nameUpperFirst: kamu kenapa gapakai bh?"\n\nKata :nameUpperFirst:, "Iya kak ini bh udah kekecilan, cuman aku pakai buat keluar rumah aja".\n\n"Udah ya kak jangan malu malu kak. Sama adek sendiri aja gitu haahaha". Gw pas liat langsung. Deg deg ser njirr. Akhirnya gw pura pura gak sengaja pegang\n\npenntilnya astaga udah keras ternyata.. Fikir gw masa adek gw sngee\n\n:nameUpperFirst: bilang. "Kak jangan pegang itunyaa gelii 😭"',
+        sensitive: true,
+    },
+    {
+        num: 3,
+        name: 'Template 4',
+        text: 'Bro, gue gemeteran. GUE GEMETERAN ANJING Gue gak pernah mau berkembangbiak dengan siapapun lebih daripada seorang :nameUpperFirst:. Badannya yang cakep, TETE GEDE, pinggul seksi dari seorang BIDADARI. Jujur aja, sakit hati kalau tau KENYATAANNYA gue GAK AKAN PERNAH BISA BUAT KAWIN SAMA DIA, ngewarisin gen gue lewat dia, dan ngebiarin dia ngelahirin anak yang sempurna dari gue. Gue rela ngelakuin APAPUN demi kesempatan buat bikin :nameUpperFirst: hamil. A P A P U N. Dan gue bener-bener gk bisa terima kenyataan. Kenapa Authornya rela bikin suatu hal yang sempurna kyk dia? Buat ngegoda kita? NGETAWAIN KITA DI DEPAN MUKA? SUMPAH BRO, GUE UDAH BENER BENER GAK TAHAN. ANJING.',
+        sensitive: true,
+    },
+    {
+        num: 4,
+        name: 'Template 5',
+        text: 'Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng :nameUpperFirst:. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot :nameUpperFirst:. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau :nameUpperFirst: jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang :nameUpperFirst:, dan INI MURNI PIKIRAN DAN PERASAAN GUE.',
+        sensitive: true,
+    },
+    {
+        num: 5,
+        name: 'Template 6',
+        text: "I would literally never stop trying to impregnate :nameUpperFirst:. Every day I would wake her up by coming in her and every night I would cum in her right before going to sleep, which I would do with my dick stuck insider her. I would take some viagra before bed just to maintain my erection so that she'll be ready in the morning when I thrust into her like an animal and slather her in kisses. Part of our wedding vows would be to have as many children as physically possible. I wouldn't even care if she's already pregnant, I'll fuck her while she's pregnant and she'll get double pregnant. I'll fill her with so much cum every day that she'll look pregnant even when she isn't (which she'll never be after we're married) I would do everything in my power to make :nameUpperFirst: as fertile as possible. I'd give her fertility drugs, I'd give her uterus massages, breast massages, I wouldn't let her go 12 hours without at least one spastic orgasm. I'll even bake her home made lactation inducing biscuits to help her get to a point of hyperlactation syndrome so that she'll be seeping out multiple quarts of milk per day. Which I will save and drink just so that I can tell her how delicious it is. I'll make her so fertile that triplets will be the minimum number she's carrying at any given time. I would literally never stop doting on her, I would respond to her every beck and call and I would cum inside her again each time she asks for something. She would be so pregnant all the time that she would literally not be able to stand up straight anymore even after menopause. Her spine would be permanently bent out of shape to accommodate a pregnant belly. Even after she can't get pregnant anymore I would just keep putting more eggs into her. I would clone her purely so that I can put fresh eggs from the clone inside her after she runs out of them. She would have so much progesterone running through her veins at any given time that even the thought of not being pregnant would seem alien to her.",
+        sensitive: true,
+    },
+    {
+        num: 6,
+        name: 'Template 7',
+        text: 'NENEN NENEN KEPENGEN NENEN SAMA :nameUpper:. TETEK GEDE NAN KENCANG MILIK :nameUpper: MEMBUATKU KEPENGEN NENEN. DIBALUT PAKAIAN KETAT YANG ADUHAI CROOOOTOTOTOTOTOT ANJING SANGE GUA BANGSAT. :nameUpper:, PLIS DENGERIN BAIK BAIK. TOLONG BUKA BAJU SEBENTAR SAJA PLISSS TOLOOONG BANGET, BIARKAN MULUT KERINGKU BISA MENGECAP NENEN :nameUpper:. BIARKAN AKU MENGENYOT NENENMU :nameUpper:. AKU RELA NGASIH SESEMBAHAN APA AJA BERAPAPUN ITU DUIT YANG AKU BAKAR KHUSUS TERKHUSUS BUATMU. TAPI TOLOOOONG BANGET BUKA BAJUMU AKU MAU NENEN. NENEN NENEEEEN NENEN :nameUpper: WANGIIII',
+        sensitive: true,
+    },
+    {
+        num: 7,
+        name: 'Template 8',
+        text: 'Sejujurnya gw ga nyangka ama tindakan lo yg ga dewasa begini. Kita udh temenan dri kecil, melalui berbagai kenangan, tapi sikap lo begini ke gw, ga habis pikir. Padahal sudah berjanji tidak mengusik hubungan satu sama lain lagi, tapi maksud tindakan mu sekarang ini apa? Tiba tiba di pagi bangun tidur lu make PP :nameUpperFirst:. Lu kira lucu begitu anjing? Make waifu pp orang seenaknya? Ngeklaim pula bangsad maksudnya apa apaan coba... Pertemanan dari kecil kita ga dihargai sama sekali. :) Gw tunggu klarifikasi lo',
+    },
+    {
+        num: 8,
+        name: 'Template 9',
+        text: ':nameUpper: AHHH :nameUpper: WANGY WANGY WANGY.... SEPONG SEPONG SEPONG 🤤 ❤️ 💔 :nameUpper: :nameUpper: AHHH AKU CROOOTT CROOTT MONCROOTT!!! 🤤 🤤 🤤 AHHH :nameUpper: KU SAYANG KU IMUT BANGET ❤️ ❤️ MANIS BANGET ❤️ ❤️ RAMBUT NYA JUGA AHHH PENGEN AKU GOSOK GOSOK NYAMPE BISA MENGALIRKAN ARUS LISTRIK BUAT NYALAIN SATU KABUPATEN 🤤 🤤 ❤️ TATAPAN :nameUpper: BEGITU MENGGODAAAAAAA....... AHHH AKU GAK KUATTTT LAGIIIII ~ AKU RELA DIENJEK INJEK SAMA :nameUpper:, AKU RELA JILATIN KETEK NYA NYAMPE AIR LIUR KU BERCUCURAN DI SEKUJUR TUBUHNYA 🤤 🤤 🤤 AKU RELA JADI BUDAK NYA SEUMUR HIDUP DEMI :nameUpper: KU SAYANG KU ❤️ ❤️ ❤️ APA KAMU BILANG? AKU FURRY? GAK GAK GAKKKK. SINI GW TONJOK DADA LU NYAMPE KEJANG KEJANG 😡 😡 😡 YEAAHHHH :nameUpper: PUNYA KU SENDIRI, ISTRI KU, BOJO KU, AHHHHH ❤️ ❤️ ❤️ AAAAAAAHHHHHHHHHHHHHHHHHHH',
+        sensitive: true,
+    },
+    {
+        num: 9,
+        name: 'Template 10',
+        text: 'GW BENAR-BENAR PENGEN JILATI KAKI :nameUpper:. GW PENGEN BANGET MENJILAT SETIAP BAGIAN KAKINYA SAMPAI AIR LIUR GW BERCUCURAN KAYAK AIR KERINGAT LALU NGENOD DENGANNYA SETIAP HARI SAMPAI TUBUH KITA MATI RASA YA TÜHAN. GW INGIN MEMBUAT ANAK-ANAK DENGAN :nameUpper: SEBANYAK SATU TÌM SEPAK BOLA DAN MEMBUAT SATU TIM SEPAK BOLA LAINNYA UNTUK MELAWAN ANAK-ANAK TIM SEPAK BOLA PERTAMA GW YANG GW BUAT SAMA :nameUpper:. GW PENGEN MASUK KE SETIAP LUBANG TUBUHNYA, MAU ITU LUBANG HIDUNG, LUBANG TELINGA, RONGGA MATA MAUPUN PUSAR, KECUALI PORI-PORI KULIT. KEMUDIAN GW AKAN MENJADIKANNYA MANUSIA YANG TIDAK BISA HIDUP KALO TIDAK GW KENTOG SETIAP HARI. DAN KALAU GUA DISEPONG GUA RELA KONTL GUA PUTUS.',
+        sensitive: true,
+    },
+    {
+        num: 10,
+        name: 'Template 11',
+        text: 'Watashi mencintai :nameUpper: dengan tulus dan penuh kasih sayang, watashi tidak tahan dengan hinaan kalian yg kalian berikan terhadap :nameUpper:. :nameUpper: selalu menangis dikamarnya setiap malam karena hinaan kalian. "shine, shine, shine" hanya kata itulah yang ada dipikiran watashi tpi watashi hanya manusia lemah yang tak berdaya jika dikroyok banyak orang. Suatu saat kemudian ada orang biadab memfitnah :nameUpper: dengan membuat video skandal lalu menyebarkannya di website pornografi. Amarah dan aura merah menyelimuti watashi tanpa disadari darah menetes dari mata watashi secepat kilat watashi menengok cermin lalu watashi melihat rambut watashi belahan menjadi putih lalu ada kagune di punggung watashi lalu sosok kaneki muncul dari dalam cermin tanpa berkata apapun dia memberikan maskernya dan langsung pergi melompati jendela. Watashi langung mencuci masker itu karena bau tengik mulut kaneki membekas di masker itu. Perut Watashi tiba tiba merasa lapar, watashi mencoba indomie buatan :nameUpper: lalu watashi muntah muntah 😖, lalu terlintas dipikiran watashi jadi berita itu benar ! ghoul memang harus memakan manusia karena watashi masih mempunya jiwa manusia akhirnya watashi memakan tangan sendiri. Meskipun tidak menghilangkan rasa lapar setidaknya ini cukup untuk berdiri lalu memangsa siapapun yang menghina istri watashi. no mercy no cry u must die, watashi tak segan segan membunuh kalian jika kalian menghina :nameUpper:.',
+    },
+];
