07f58cb029d245eb9eff1026fe5b233078a81f3c
Gifaldy Azka <me@falcxxdev.ml>
2022-08-06 20:38:36 +0700

    feat: receive emit from input component

--- src/components/NamaWaifu.vue
+++ src/components/NamaWaifu.vue
@@ -1,15 +1,28 @@
-<template>
-    <p>1. Sebutkan nama waifu 2D mu</p>
-    <Input placeholder="Keqing, Rem, etc." />
-</template>
-
 <script lang="ts">
 import Input from '../shared/Input.vue';
 
 export default {
     name: 'NamaWaifu',
     components: { Input },
+    data() {
+        return {
+            inputValue: null,
+        };
+    },
+    methods: {
+        handleInput(val) {
+            this.inputValue = val;
+        },
+        sendInput(e) {
+            this.$emit('sendInput', e);
+        },
+    },
 };
 </script>
+
+<template>
+    <p>1. Sebutkan nama waifu 2D mu</p>
+    <Input placeholder="Keqing, Rem, etc." @input="sendInput(inputValue)" @receiveInput="handleInput" />
+</template>
 
 <style lang="scss"></style>
