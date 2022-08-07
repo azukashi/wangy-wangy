d7b424c823e943ff3cc83cfe82a03361fadbd3b4
Gifaldy Azka <me@falcxxdev.ml>
2022-08-07 19:24:25 +0700

    feat: use vue-sweetalert2

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import { createApp } from 'vue';
 import './types/vue.d';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+createApp(App).use(VueSweetalert2).mount('#app');
