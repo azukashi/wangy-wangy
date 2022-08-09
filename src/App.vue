a2284a8503207d39a1a4213b1b74a24552a41893
Gifaldy Azka <me@falcxxdev.ml>
2022-08-09 19:20:17 +0700

    feat: import & use footer component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
         <NamaWaifu @sendInput="handleInput" />
         <TemplateBtn :waifu="waifu" @result="handleResult" />
         <Result :result="result" />
+        <Footer />
     </Container>
 </template>
 
@@ -13,11 +14,12 @@ import Title from './components/Title.vue';
 import NamaWaifu from './components/NamaWaifu.vue';
 import TemplateBtn from './components/TemplateBtn.vue';
 import Result from './components/Result.vue';
+import Footer from './components/Footer.vue';
 import NProgress from 'nprogress';
 
 export default {
     name: 'App',
-    components: { Container, Title, NamaWaifu, TemplateBtn, Result },
+    components: { Container, Title, NamaWaifu, TemplateBtn, Result, Footer },
     mounted() {
         NProgress.start();
         window.addEventListener('load', () => NProgress.done());
