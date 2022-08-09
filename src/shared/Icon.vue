dd05aa786af7d24147748f33028a10276d9a6c64
Gifaldy Azka <me@falcxxdev.ml>
2022-08-09 19:17:40 +0700

    feat: add shared icon component

---
+++ src/shared/Icon.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Icon } from '@iconify/vue';
+
+export default {
+    name: 'Icon',
+    components: {
+        Iconify: Icon,
+    },
+    props: ['name'],
+};
+</script>
+
+<template>
+    <Iconify :icon="name" width="20" height="20" />
+</template>
