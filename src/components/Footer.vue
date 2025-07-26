a4fef180f864fb12147b47396ad6eec21c4f881b
Azka <falcxxdev@pm.me>
2025-07-26 07:17:02 +0000

    chore(footer): add support link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,11 +12,23 @@ export default {
     <footer>
         <div class="separator"></div>
         <Container>
-            <p>Made with ❤️ by <b>Azukashiic</b></p>
-            <span>|</span>
-            <a href="https://github.com/azukashi" target="_blank">
-                <Icon name="bxl:github" width="24" height="24" />
-            </a>
+            <div class="author">
+                <p>Made with ❤️ by <b>Azukashiic</b></p>
+                <span>|</span>
+                <a href="https://github.com/azukashi" target="_blank">
+                    <Icon name="bxl:github" width="24" height="24" />
+                </a>
+            </div>
+            <div class="support">
+                <p>Did you like this project? Support me on</p>
+                <a
+                    href="https://trakteer.id/azukashiic"
+                    style="height: auto !important; color: #be212f; font-weight: 700"
+                    target="_blank"
+                    >Trakteer</a
+                >
+                <span>!</span>
+            </div>
         </Container>
     </footer>
 </template>
@@ -31,9 +43,22 @@ footer {
     margin-top: auto;
     .container {
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
         gap: 5px;
+        .author {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
+        }
+        .support {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
+        }
     }
     .separator {
         border-top: 1px solid $dark-text;
